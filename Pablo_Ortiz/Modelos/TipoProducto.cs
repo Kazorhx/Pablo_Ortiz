e6935a258a3d31d5c028b15f9536d5698afc2172
Kazor <pablo.ortiz@cftsa.cl>
2024-12-10 02:06:25 -0300

    Instalacion de paquetes nugget, creacion de archivo DBContext y modelos para Proveedor, TipoProducto y Ubicación

---
+++ Pablo_Ortiz/Modelos/TipoProducto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pablo_Ortiz.Modelos
+{
+    public partial class TipoProducto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string? Descripcion { get; set; }
+        public string? Estado { get; set; }
+    }
+}
