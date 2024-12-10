90dfc72b890b20be80ab38f39c49e460bad08167
Kazor <pablo.ortiz@cftsa.cl>
2024-12-10 01:27:40 -0300

    Agregar archivos de proyecto.

---
+++ Pablo_Ortiz/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pablo_Ortiz.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
